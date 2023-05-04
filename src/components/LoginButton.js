2eb802f2d61e749711d7a52db8dbc3471e85013f
Hubert Bourget <hubbourget@hotmail.com>
2023-05-04 15:55:58 -0600

    May the 4th release fixed auth redirectUri

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -8,7 +8,7 @@ const LoginButton = () => {
     return (
         !isAuthenticated && (
             <SingupButton onClick={() => loginWithRedirect({
-                redirectUri: "http://sacredsound.app/studio", // Change this for dev: "http://localhost:3000/studio"
+                redirectUri: "https://sacredsound.app/studio", // Change this for dev: "http://localhost:3000/studio"
 })}>
                 <LoginFonts>Become a Resident Artist</LoginFonts>
             </SingupButton>
