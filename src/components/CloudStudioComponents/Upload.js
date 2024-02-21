dd354f1b0dfdcd025ca91eb403cf5f6eb436a7c1
Hubert Bourget <hubbourget@hotmail.com>
2024-02-20 20:45:11 -0600

    Making it better

--- src/components/CloudStudioComponents/Upload.js
+++ src/components/CloudStudioComponents/Upload.js
@@ -239,7 +239,7 @@ useEffect(() => {
     const handleUpdateReviewStatus = async () => {
         // setIsUpdating(true);
         const fileIds = fileUploadsArray.map(file => file.videoId);
-        console.log("file :", file);
+        console.log("fileIds :", fileIds);
 
         console.log("Starting the update process for review status.");
 
